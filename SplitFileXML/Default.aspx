797a4fe2f08a57c69c323db05a13cb4ef3c04c42
Mohamed Amine Ben Noureddine <mohamed-amine.ben-nourddine@talan.com>
2021-12-02 12:29:11 +0100

    first commit

---
+++ SplitFileXML/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SplitFileXML.Default" ValidateRequest="false" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>App to split an XML File into multiple XML Files  </h1>
+            <table>
+                <tr>
+                    <td><asp:TextBox ID="TxtFile" runat="server" TextMode="MultiLine" Rows="40" Width="500px" ></asp:TextBox></td>
+               
+                    <td><asp:TextBox ID="TxtFileSplit" runat="server" TextMode="MultiLine" Rows="40" Width="500px"></asp:TextBox></td>
+                </tr>
+            </table>
+          
+            <br /><br />
+            <asp:Label ID="Label1" runat="server" Text="Give the tag for the divison:"></asp:Label>
+            <asp:TextBox ID="TxtTag" runat="server"></asp:TextBox>
+            <asp:Button ID="BtnSplit" runat="server" Text="Split" OnClick="BtnSplit_Click" /> <br /><br />
+                <br /><br />
+
+            &nbsp; &nbsp;   <asp:Label ID="LblShowMessage" runat="server" ForeColor="Blue" Visible="False"></asp:Label>
+            <br />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ SplitFileXML/Default.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace SplitFileXML
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string pathFile = ConfigurationManager.AppSettings["PathFile"];
+
+         string pathFileOut = ConfigurationManager.AppSettings["PathFileOut"];
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            XDocument doc = XDocument.Load(pathFile);
+            XDeclaration declaration = doc.Declaration;
+            TxtFile.Text = declaration.ToString()+ "\n"+  doc.ToString();
+        }
+
+        protected void BtnSplit_Click(object sender, EventArgs e)
+        {
+            SplitFileXML(pathFile);
+            LblShowMessage.Text = "file XML is splitted ";
+            LblShowMessage.ForeColor = Color.Green;
+            LblShowMessage.Visible = true;
+        }
+
+        private void SplitFileXML(string url)
+        {
+            XDocument doc = XDocument.Load(url);
+
+            XDeclaration declaration = doc.Declaration;
+            string nouedToSplit = TxtTag.Text;
+
+           
+            IEnumerable<XDocument> newDocs = doc.Descendants(nouedToSplit)
+                 .Select(d => new XDocument(new XElement(doc.Root.Name.ToString(), d)));
+
+           
+            XDocument[] tabDoc = newDocs.ToArray();
+            StringBuilder text = new StringBuilder();
+            for (int i = 0 ; i < tabDoc.Length; i++)    
+            {
+                tabDoc[i].Declaration = declaration;
+                tabDoc[i].Save(pathFileOut +" noued  "+ (i+1)+ ".xml" );
+
+                text.Append("\n---------------------------------------------------------------\n");
+
+                text.Append(tabDoc[i].Declaration.ToString() + "\n");
+                text.Append(tabDoc[i].ToString());
+
+            }
+            TxtFileSplit.Text = text.ToString();
+
+        }
+
+      
+
+    }
+}
